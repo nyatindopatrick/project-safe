ccc5b8b2034de2f5cb9e09379f5f631a1be81bf4
Patrick Nyatindo <nyatindopatrick@gmail.com>
2019-09-17 23:34:02 +0300

    add contact redirect and onsubmit

--- routes/contact.js
+++ routes/contact.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 
 router.post('/', (req,res)=>{
     sgMail.setApiKey(process.env.SG_KEY);
-    console.log(process.env.SG_KEY)
     const msg = {
         to: ['nyatindopatrick@gmail.com', req.body.email],
         from: 'support@fikasafe.com',
@@ -14,9 +13,10 @@ router.post('/', (req,res)=>{
         <br><br>${req.body.message}</strong>`,
     };
     sgMail.send(msg);
-    res.status(200)
+    res.status(200);
+    req.flash('success_msg', 'Message sent sucessfully!');
+    res.redirect('/')
     console.log("success");
-    req.flash('success_msg', 'Email sent sucessfully!');
 });
 
 module.exports = router;
