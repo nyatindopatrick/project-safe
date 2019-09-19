6db993508ba61de2a3e0007d564116b7276d2556
Patrick Nyatindo <nyatindopatrick@gmail.com>
2019-09-19 23:33:10 +0300

    change logout redirect route

--- routes/users.js
+++ routes/users.js
@@ -292,7 +292,7 @@ router.post('/login', (req, res, next) => {
 router.get('/logout', (req, res) => {
   req.logout();
   req.flash('success_msg', 'You are logged out');
-  res.redirect('/users/login');
+  res.redirect('/');
 });
 
 
